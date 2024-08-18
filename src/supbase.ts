9ba1381f0c694dc8bbe0018003665b554f9500d3
Andrian Barbulat <andrian.barbulat@gmail.com>
2024-08-18 20:30:36 +0100

    update file

--- src/supbase.ts
+++ src/supbase.ts
@@ -1,4 +1,4 @@
 export const supabaseConfig = {
-  url: process.env.REACT_APP_SUPABASE_URL,
-  anonKey: process.env.REACT_APP_SUPABASE_ANON_KEY
-};
+    url: 'https://wbzxjkoeievktfrsgsbo.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indienhqa29laWV2a3RmcnNnc2JvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjM1NzU3NDgsImV4cCI6MjAzOTE1MTc0OH0.k0oMNWfRUYRYsRmXrC7zhUyq7yAu-m54bvuhoFwBtJQ'
+  };
